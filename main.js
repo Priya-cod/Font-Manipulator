3f0b33fa761be729781a127b14463082a00700d7
Priya-cod <72642192+Priya-cod@users.noreply.github.com>
2021-05-15 13:21:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 function setup()
 {
-    canvas = createCanvas(400, 400);
-    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('poseNet Is Initialized!');
+}
+
+function draw() {
+    background('#969A97')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
 }
